0f1879cddffa369c509c8c388d7767072cdb3e11
Vanessa Lima <vanessalima.f@hotmail.com>
2023-03-21 11:03:21 -0300

    feat: controlled search component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
+
+const navbarStyle = {
+    bg: 'light'
+};
 
 const Header = ({ title }) => {
-    return (
-        <Navbar bg="light" variant="light">
-            <Navbar.Brand href="/">{title}</Navbar.Brand>
-        </Navbar>
-    )
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
