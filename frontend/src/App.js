70fcdf1d4038c3b59309625f9a834aa02e66dc37
Vanessa Lima <vanessalima.f@hotmail.com>
2023-03-22 17:23:08 -0300

    feat: display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
-
-  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -19,7 +19,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        setImages([data, ...images]);
+        setImages([{ ...data, title: word }, ...images]);
       })
       .catch((err) => {
         console.log(err);
@@ -27,10 +27,23 @@ const App = () => {
     setWord(''); //cleaning search tool box after press search
   };
 
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id)); //filtering images that are not being deleted
+  };
+
   return (
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
