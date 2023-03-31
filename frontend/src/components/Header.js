91b64f9f4c029fc16a28eeb0bfd5843f46ea883e
Vanessa Lima <vanessalima.f@hotmail.com>
2023-03-31 15:17:41 -0300

    feat: added text logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 
 const navbarStyle = {
-  bg: 'light',
+  backgroundColor: '#eeadad',
 };
 
 const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Logo style={{ maxWidth: '13rem', maxHeight: '2rem' }} />
       </Container>
     </Navbar>
   );
